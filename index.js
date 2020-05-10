f304277aee5a0f8149f0efc1bfc08d6829ca7b8f
DavidSabino11 <david.sabino17@gmail.com>
2020-05-10 11:44:30 -0300

    versão 1.0

---
+++ index.js
@@ -0,0 +1,35 @@
+const endereco = 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72';
+
+async function getDados(url){
+  const response = await fetch(url);
+  return await response.json();
+}
+
+function createCard(element) {
+  document.getElementById('quartos').innerHTML += `
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="${element.photo}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${element.property_type}</h5>
+            <p class="card-text"> ${element.name} <br><b>R$: ${element.price}</b></p>
+        </div>
+      </div>
+    </div>
+    `
+  }
+
+  function popular(dados) {
+    dados.forEach(element => {
+      createCard(element);
+    })
+  }
+
+
+window.onload = async () => {
+
+  const dados = await getDados(endereco);
+
+  popular(dados);
+  
+}
